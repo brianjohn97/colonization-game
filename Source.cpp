1cf3278fc8badab7ce096c320b67e8d6d73b54f0
brianjohn97 <31637076+brianjohn97@users.noreply.github.com>
2023-05-04 17:05:03 -0700

    updated functions.

--- Source.cpp
+++ Source.cpp
@@ -2,22 +2,27 @@
 #include <pthread.h>
 #include <vector>
 #include <fstream>
+#include <stdlib.h>
+#include <ctime>
+#include <cstring>
 
 using namespace std;
 
 //global variables
-int teamOnePlayers = 0;
-int teamTwoPlayers = 0;
-int row = 10;
-int col = 10;
-vector<vector<int>> map(row, vector<int>(col));
+int teamOnePlayers;
+int teamTwoPlayers;
+int row;
+int col;
+vector<vector<string>> board(row, vector<string>(col));
+string winner = "0";
+vector<int> places;
 
 void saveMap(const char* filename){
     //open the file in binary mode
     ofstream file(filename, ios::binary);
 
     //write the map data to the file
-    for(const auto& width : map){
+    for(const auto& width : board){
         file.write(reinterpret_cast<const char*> (&width[0]), col * sizeof(int));
     }
 
@@ -29,7 +34,7 @@ void loadMap(const char* filename){
     ifstream file(filename, ios::binary);
 
     //read the map data from the file
-    for(auto& width : map){
+    for(auto& width : board){
         file.read(reinterpret_cast<char*> (&width[0]), col * sizeof(int));
     }
 }
@@ -109,29 +114,167 @@ void getPlayersAndBoard(int  argc, char **argv) {
         exit(0);
     }
 }
+void updateBoard(){
 
-int main(int argc, char * argv[]) {
-    //getPlayersAndBoard(argc, argv);
-
-    map[3][5] = 1;
-    map[7][2] = 2;
-
-    saveMap("map.bin");
-
-    map.clear();
-
-
-    map.resize(row, vector<int>(col));
-
-    loadMap("map.bin");
-
-    for(const auto& width : map){
-        for (int value : width){
+    saveMap("board.bin");
+    board.clear();
+    board.resize(row, vector<string>(col));
+    loadMap("board.bin");
+    for(const auto& width : board){
+        for (string value : width){
             cout << value << " ";
         }
         cout << endl;
     }
+}
+void placingPlayers(int player, int x, int y){
+    switch (player)
+    {
+    case 1:
+        board[x][y] = "P1";
+        break;
+    case 2:
+        board[x][y] = "P2";
+        break;
+    case 3:
+        board[x][y] = "P3";
+        break;
+    case 4:
+        board[x][y] = "P4";
+        break;
+    case 5:
+        board[x][y] = "P5";
+        break;
+    case 6:
+        board[x][y] = "P6";
+        break;
+    case 7:
+        board[x][y] = "P7";
+        break;
+    case 8:
+        board[x][y] = "P8";
+        break;
+    case 9:
+        board[x][y] = "P9";
+        break;
+    case 10:
+        board[x][y] = "P10";
+        break;
+    case 11:
+        board[x][y] = "P11";
+        break;
+    case 12:
+        board[x][y] = "P12";
+        break;
+    case 13:
+        board[x][y] = "P13";
+        break;
+    case 14:    
+        board[x][y] = "P14";
+        break;
+    case 15:
+        board[x][y] = "P15";
+        break;
+    case 16:
+        board[x][y] = "P16";
+        break;
+    case 17:
+        board[x][y] = "P17";
+        break;
+    case 18:
+        board[x][y] = "P18";
+        break;
+    case 19:
+        board[x][y] = "P19";
+        break;
+    case 20:
+        board[x][y] = "P20";
+        break;  
+    default:
+        break;
+    }
+}
+void printStart(){
+    
+    
+
+    srand(static_cast<unsigned int>(time(0)));
+    cout << "\nThe goal of the colonization game is to" 
+           "\nconquer the rectangular map from the opposite team!\n\n";
+    for (int i = 1; i <= (teamOnePlayers + teamTwoPlayers); i++){
+        int x = rand() % 10;
+        int y = rand() % 10;
+        for (int i = 0; i < places.size(); i++){
+            if(x == places[i] || y == places[i]){
+                continue;
+            }
+        }
+        
+        placingPlayers(i, x, y);
+    }
+    for(const auto& width : board){
+        for (string value : width){
+            cout << "[ " << value << " ]";
+        }
+        cout << endl;
+    }
+    
+    
+}
+void missile(){
+
+}
+void * player(void * threadID){
+    
+    return (void*)0;
+}
+void * supervisor(void * ThreadID){
+    //will check if the game has ended
+    
+    //send signal to the players that the game is over and to stop shooting missiles
+
+    //pronounce winner
+    if(winner == "1"){
+        cout << "\nTeam 1 has won the game! If you would would to retry restart the game with the same parameters\n";
+    }else if(winner == "2"){
+        cout << "\nTeam 2 has won the game! If you would would to retry restart the game with the same parameters\n";
+    }
+    return (void*)0;
+}
+int main(int argc, char * argv[]) {
+    cout << "hertjh";
+    
+    getPlayersAndBoard(argc, argv);
+    cout << "team 1 players: " << teamOnePlayers << endl;
+    cout << "team 2 players: " << teamTwoPlayers << endl;
+    cout << "rows: " << row << endl;
+    cout << "columns: " << col << endl;
+
+    /*
+    for (int i=0;i<row;i++){
+        cout << "first for loop";
+        for (int j = 0; j < col; j++){
+            cout << "second for loop";
+            board[i][j] = "00";
+        }
+        
+    }
 
 
+    printStart();
+
+    
+    
+    
+    //pthread_t myThreads[10];
+
+    /*
+    for (int i = 1; i <= teamOnePlayers + teamTwoPlayers; i++){
+        int rval = pthread_create(&myThreads[i], NULL, player, (void *)(long)i);    
+    }
+    
+    
+
+    board.clear();*/
     return 0;
 }
